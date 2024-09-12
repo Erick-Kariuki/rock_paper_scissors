406961d8f8a090636674688976c59998920ec11b
Erick Kariuki <ithandierick4@gmail.com>
2024-09-12 23:59:10 +0300

    Add rock-paper.html and rock-paper.js and code to play rock paper scissors game

---
+++ rock-paper.js
@@ -0,0 +1,76 @@
+
+function getComputerChoice(){
+    let num_one = "rock"
+    let num_two = "paper"
+    let num_three = "scissors"
+    let compChoice = Math.floor(Math.random() * 3)
+    if (compChoice === 0){
+        return num_one
+    }
+    else if (compChoice === 1){
+        return num_two
+    }
+    else{
+        return num_three
+    }
+}
+console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let answer = prompt("Enter your choice. :Either rock, paper or scissors").toLowerCase()
+    return answer
+
+}
+console.log(getHumanChoice())
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice, computerChoice){
+    let win1 = "You win! Rock beats scissor"
+    let win2 = "You win! Paper beats rock"
+    let win3 = "You win! Scissor beats paper"
+
+    let lose1 = "You lose! Rock beats scissor"
+    let lose2 = "You lose! Paper beats rock"
+    let lose3 = "You lose! Scissor beats paper"
+
+    let draw = "You draw!"
+
+    if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log(win1)
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        console.log(win2)
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        console.log(win3)
+    }
+    else if(humanChoice === "rock" && computerChoice === "paper"){
+        console.log(lose2)
+    }
+    else if(humanChoice === "paper" && computerChoice === "scissors"){
+        console.log(lose3)
+    }
+    else if(humanChoice === "scissors" && computerChoice === "rock"){
+        console.log(lose1)
+    }
+    else if (humanChoice == computerChoice){
+        console.log(draw)
+    }
+
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
+
+
+
+
+
+
+
+
+
