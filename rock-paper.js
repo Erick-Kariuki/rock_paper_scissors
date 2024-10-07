24f370acaaac8de6dd0419a07895e9b979aa5f60
Erick Kariuki <ithandierick4@gmail.com>
2024-10-07 11:21:59 +0300

    modify the getHumanChoice() method to have one return statement

--- rock-paper.js
+++ rock-paper.js
@@ -1,70 +1,75 @@
 
 function getComputerChoice(){
+    
+    let computerAnswer = null
+
     let num_one = "rock"
     let num_two = "paper"
     let num_three = "scissors"
+
     let compChoice = Math.floor(Math.random() * 3)
     if (compChoice === 0){
-        return num_one
+        computerAnswer = num_one
     }
     else if (compChoice === 1){
-        return num_two
+        computerAnswer = num_two
     }
     else{
-        return num_three
+        computerAnswer = num_three
     }
+    return "Computer choice: "+computerAnswer    
 }
 console.log(getComputerChoice())
 
-function getHumanChoice(){
+ function getHumanChoice(){
     let answer = prompt("Enter your choice. :Either rock, paper or scissors").toLowerCase()
-    return answer
+    return "Your choice: "+answer
 
-}
-console.log(getHumanChoice())
+ }
+ console.log(getHumanChoice())
 
-let humanScore = 0
-let computerScore = 0
+// let humanScore = 0
+// let computerScore = 0
 
-function playRound(humanChoice, computerChoice){
-    let win1 = "You win! Rock beats scissor"
-    let win2 = "You win! Paper beats rock"
-    let win3 = "You win! Scissor beats paper"
+// function playRound(humanChoice, computerChoice){
+//     let win1 = "You win! Rock beats scissor"
+//     let win2 = "You win! Paper beats rock"
+//     let win3 = "You win! Scissor beats paper"
 
-    let lose1 = "You lose! Rock beats scissor"
-    let lose2 = "You lose! Paper beats rock"
-    let lose3 = "You lose! Scissor beats paper"
+//     let lose1 = "You lose! Rock beats scissor"
+//     let lose2 = "You lose! Paper beats rock"
+//     let lose3 = "You lose! Scissor beats paper"
 
-    let draw = "You draw!"
+//     let draw = "You draw!"
 
-    if(humanChoice === "rock" && computerChoice === "scissors"){
-        console.log(win1)
-    }
-    else if(humanChoice === "paper" && computerChoice === "rock"){
-        console.log(win2)
-    }
-    else if(humanChoice === "scissors" && computerChoice === "paper"){
-        console.log(win3)
-    }
-    else if(humanChoice === "rock" && computerChoice === "paper"){
-        console.log(lose2)
-    }
-    else if(humanChoice === "paper" && computerChoice === "scissors"){
-        console.log(lose3)
-    }
-    else if(humanChoice === "scissors" && computerChoice === "rock"){
-        console.log(lose1)
-    }
-    else if (humanChoice == computerChoice){
-        console.log(draw)
-    }
+//     if(humanChoice === "rock" && computerChoice === "scissors"){
+//         console.log(win1)
+//     }
+//     else if(humanChoice === "paper" && computerChoice === "rock"){
+//         console.log(win2)
+//     }
+//     else if(humanChoice === "scissors" && computerChoice === "paper"){
+//         console.log(win3)
+//     }
+//     else if(humanChoice === "rock" && computerChoice === "paper"){
+//         console.log(lose2)
+//     }
+//     else if(humanChoice === "paper" && computerChoice === "scissors"){
+//         console.log(lose3)
+//     }
+//     else if(humanChoice === "scissors" && computerChoice === "rock"){
+//         console.log(lose1)
+//     }
+//     else if (humanChoice == computerChoice){
+//         console.log(draw)
+//     }
 
-}
+// }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
+// const humanSelection = getHumanChoice()
+// const computerSelection = getComputerChoice()
 
-playRound(humanSelection, computerSelection)
+// playRound(humanSelection, computerSelection)
 
 
 
